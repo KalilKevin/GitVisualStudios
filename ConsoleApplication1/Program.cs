19d12b32c132cc63017710da368ad25aac2a3350
KalilKevin <kalilmaldonado.km@gmail.com>
2019-08-23 20:30:00 -0400

    se cambia la tercera frase impresa

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Segundo cambio");
-            Console.WriteLine("Tercer cambio");
+            Console.WriteLine("Tercer pinche cambio");
         }
     }
 }
